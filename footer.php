e7073755156f13678e97df8515d50df59a3679ce
SahiL iFtekhar <70858289+sahiliftekhar@users.noreply.github.com>
2020-09-30 21:12:58 +0530

    Footer file added

---
+++ footer.php
@@ -0,0 +1,12 @@
+<hr>
+
+<footer>
+    <p class="p-3 bg-dark text-white text-center">@soul_enterprises</p>
+</footer>
+</div> <!-- /container -->
+
+
+<script type="text/javascript" src="./bootstrap/js/jquery-2.1.4.min.js"></script>
+<script type="text/javascript" src="./bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
